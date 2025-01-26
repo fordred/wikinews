aee5cc306506f178f2545e15fbe0945756fe00dc
Josh <ford.red@gmail.com>
2025-01-27 02:59:41 +0800

    fix script

--- wikipedia_news_downloader.py
+++ wikipedia_news_downloader.py
@@ -64,7 +64,7 @@ def download_wikipedia_news(date, logger):
         front_matter = "---\n"
         front_matter += "layout: post\n"
         front_matter += "title: " + date.strftime("%Y %B %d") + "\n"
-        front_matter += "date: " + date.strftime("%Y-%m-%d %H:%M:%S %z") + "\n"
+        front_matter += "date: " + date.strftime("%Y-%m-%d 00:00:00 +0000") + "\n"
         front_matter += "---\n\n"
         front_matter += f"# {date.strftime('%Y %B %d')}\n\n"
         markdown_text = convert_to_markdown(content, logger)
