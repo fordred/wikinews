de644a19897b1f20bf7eac1697c57038ea2e2779
Josh <ford.red@gmail.com>
2025-06-16 00:59:53 +0800

    Refactor shebang in wikipedia_news_downloader.py to simplify execution environment

--- wikipedia_news_downloader.py
+++ wikipedia_news_downloader.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env -S uv run --script
-# /// script
-# requires-python = "==3.13.*"
-# dependencies = [
-#     "markitdown",
-# ]
-# ///
+#!/usr/bin/env -S uv run
 
 from typing import Optional, Set
 import argparse
